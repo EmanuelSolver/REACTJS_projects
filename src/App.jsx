1adb9ed7b62cb7df711c465880d2a8cd36a3e188
Emanuel Njiru <emanuelmutuiri@gmail.com>
2023-05-17 10:14:13 +0300

    worked on background

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 
-// function App() {
-//   //const [count, setCount] = useState(0)
-  
-//   return (
-//     <>
-//      {/* calling our child component */}
-//       <MyComp myimage="./vite.svg" myname="Emanuel" statement="I'm developing a new App" position='software Engineer'/>
-//       <MyComp myimage="./react.svg" myname="Pentagon" statement="working in a new project" position='program manager'/>
-      
-//     </>
-//   )
-// }
 
-// //creating a new component
-// const MyComp = ({statement, position, myname, myimage})=>{
-//   return(
-//     <div>
-//       <p>Hey, {statement}</p>
-//       <h2>I'm a {position} at theJitu</h2>
-//       <img src={myimage} alt="vite" />
-//       <button style={{backgroundColor:'greenyellow', color: '#000'}}>{myname}</button>
-//     </div>
-//   );
- 
-// }
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return( <>  
-//       <h1>My name is {this.props.myname}</h1>
-//     </>)
-//   }
-// }
 
 const obj = [{
   blogNames : 'Health',
@@ -62,7 +25,7 @@ const obj = [{
 
 function App() {
   return (
-    <div style={{display: "flex", flexDirection:"column", backgroundColor:"grey", width:"70%"}}>  
+    <div style={{display: "flex", flexDirection:"column", width:"70%"}}>  
 
         <h2 style={{textAlign: "center"}}>Productivity and Its Boosters</h2>
  
@@ -71,9 +34,9 @@ function App() {
         <Part myimage="./vite.svg" blogName= "Weather" content="Weather is one of the things that boost productivity"/>
         <Part myimage="./react.svg" blogName= "Education" content="Improvement of livelihood is anchored on good education"/>
         <Footer />
-        <Aside />
-        {/* <List />  */}
+        <Aside style={{display:""}} />
     </div> 
+
   )
 }
 
@@ -112,6 +75,5 @@ const Aside =()=>{
     
   )
 }
-
 
 export default App
