c0f88f62ab521957806557e190cdbe084de07275
Emanuel Njiru <emanuelmutuiri@gmail.com>
2023-05-17 09:12:04 +0300

    styling the App

--- present/src/App.jsx
+++ present/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 // function App() {
 //   //const [count, setCount] = useState(0)
@@ -41,25 +41,45 @@ import './App.css'
 //   }
 // }
 
+const obj = [{
+  blogNames : 'Health',
+  content : 'Everyone deserves a healthy living'
+},
+{
+  blogNames : 'Environment',
+  content : 'Everyone deserves clean environment'
+},
+{
+  blogNames : 'Weather',
+  content : 'Weather is one of the things that boost productivity'
+},
+{
+  blogNames : 'Education',
+  content : 'Improvement of livelihood is anchored on good education'
+}
+];
+
+
 function App() {
   return (
-    <div style={{backgroundColor:"grey"}}>  
-        <h2 style={{textAlign: "center"}}>Productivity and Its Boosters</h2>
+    <div style={{display: "flex", flexDirection:"column", backgroundColor:"grey", width:"70%"}}>  
 
+        <h2 style={{textAlign: "center"}}>Productivity and Its Boosters</h2>
+ 
         <Part myimage="./vite.svg" blogName= "Health" content="Everyone deserves a healthy living"/>
         <Part myimage="./react.svg" blogName= "Environment" content="Everyone deserves clean environment"/>
         <Part myimage="./vite.svg" blogName= "Weather" content="Weather is one of the things that boost productivity"/>
         <Part myimage="./react.svg" blogName= "Education" content="Improvement of livelihood is anchored on good education"/>
-        <p>Designed by @Njiru_Emanuel</p>
-        <p>Created on {new Date().toLocaleDateString()}</p>
-        {/* <List /> */}
-    </div>
+        <Footer />
+        <Aside />
+        {/* <List />  */}
+    </div> 
   )
 }
 
 const Part = ({blogName, content, myimage}) =>{
   return (
-    <div>
+    <div >
         <img src={myimage} alt="vite" />
         <h3>{blogName}</h3>
         <p>{content}</p>
@@ -67,23 +87,31 @@ const Part = ({blogName, content, myimage}) =>{
   )
 }
 
-const titles = ["Health","Environments","Weather"];
+const Footer = ()=>{
+  return (
+    <div style={{display:"flex", justifyContent:"space-between", backgroundColor:'pink'}}>
+      <p>Designed by @Njiru_Emanuel</p>
+      <p>Created on {new Date().toLocaleDateString()}</p>
+    </div>
+  )
+}
 
-// const List = () =>{
-//   return(
-//     <div style={{display: "flex"}}>
-//       <h3>Highlights</h3>
-//       <ul>
-//         titles.map(titles =>(title){
-//           <li>See {title}</li>
-//         })
-//       </ul>
-//     </div>
+const Aside =()=>{
+  return(
+    <div style={{float:'right'}}>
+      <h2 style={{textAlign:"center"}}>Highlights</h2>
+
+      {
+        obj.map((item, index)=>{
+            return <h3>{item.blogNames}</h3>;
+            
+          
+        })
+      }
+    </div>
     
+  )
+}
 
-//   )
-// }
-
-<App />
 
 export default App
